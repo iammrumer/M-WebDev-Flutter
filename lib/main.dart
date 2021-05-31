9088840eee4a9ff055c472f8227bc1e60652909f
unknown <umermuhammadiqbal@gmail.com>
2021-05-31 19:17:44 +0500

    Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,63 @@
 import 'package:flutter/material.dart';
 
-
 void main(){
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text("Umer Muhammad Iqbal Email: umermuhammadiqbal@gmail.com"),
-      ),
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Login Form")) ,),
+
+          body: Center(
+            child: Column(children: [
+              SizedBox(height: 50,),
+
+              Container(
+                width: 300,
+                height: 50,
+                child: TextField(
+                 decoration: new InputDecoration(
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.greenAccent, width: 2.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue, width: 2.0),
+            ),
+            hintText: 'Email',
+        ),
+                )),
+                SizedBox(height: 20,),
+
+
+                Container(
+                width: 300,
+                child: TextField(
+                  decoration: new InputDecoration(
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.greenAccent, width: 2.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue, width: 2.0),
+            ),
+            hintText: 'Password',
+        ),
+
+
+
+                )),
+                SizedBox(height: 30,),
+
+                ElevatedButton(onPressed: (){}, child: Text("Login"),)
+
+            ],),
+          )
+      )
       
     );
   }
 }
-
-
-
